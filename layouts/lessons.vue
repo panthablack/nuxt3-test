9050be196c6196b41d181e516a2e0065a4f4fb34
James Randall <james@manmachineltd.com>
2023-12-03 22:44:21 +1100

    added layouts

---
+++ layouts/lessons.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
+    <div class="prose mb-12">
+      <h1>
+        <span class="font-medium">
+          <span class="font-bold">{{ course.title }}</span>
+        </span>
+      </h1>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="js">
+const { state: course } = useCourse()
+</script>
--- pages/course.vue
+++ pages/course.vue
@@ -1,19 +1,10 @@
 <template>
-  <div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
-    <div class="prose mb-12">
-      <h1>
-        <span class="font-medium">
-          <span class="font-bold">{{ course.title }}</span>
-        </span>
-      </h1>
-    </div>
-    <div class="flex flex-row justify-center flex-grow">
-      <ChapterList />
-      <NuxtPage />
-    </div>
+  <div class="flex flex-row justify-center flex-grow">
+    <ChapterList />
+    <NuxtPage />
   </div>
 </template>
 
 <script setup lang="js">
-const { state: course } = useCourse()
+definePageMeta({ layout: 'lessons' })
 </script>
